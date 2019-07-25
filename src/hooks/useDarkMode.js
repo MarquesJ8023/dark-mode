f4102ddd8b940a72392d3dd8cc6e0b5430577e42
Marques Johnson <marques.johnson1992@gmail.com>
2019-07-25 17:50:40 -0400

    first commit

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useDarkMode = () => {
+
+    const [darkMode, setDarkMode] = useLocalStorage(1, false);
+
+    useEffect(() => {
+        
+        if (darkMode === true) {
+
+            document.querySelector('body').classList.add('dark-mode');
+
+        } else {
+
+            document.querySelector('body').classList.remove('dark-mode');
+
+        };
+
+    }, [darkMode]);
+
+    return [darkMode, setDarkMode];
+
+}
